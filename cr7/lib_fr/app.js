beccacbf7931465a54f6170ad28ebd6e31ac8ad9
nba-stream <94798568+nba-stream@users.noreply.github.com>
2023-02-26 15:41:32 +0500

    Add files via upload

--- cr7/lib_fr/app.js
+++ cr7/lib_fr/app.js
@@ -1,22 +1,15 @@
-$('a').mouseenter(function(){
-    $(this).css('color', 'rgb(186, 186, 186)')
-})
 
-$('a').mouseleave(function(){
-    $(this).css('color', 'white')
-})
-
-$('button').click(function(){
+$('.copybtn').click(function(){
     $(this).addClass('bg-danger')
 })
 
 
-$('button').mouseenter(function(){
+$('.copybtn').mouseenter(function(){
     $(this).removeClass('bg-secondary')
     $(this).addClass('bg-primary')
 })
 
-$('button').mouseleave(function(){
+$('.copybtn').mouseleave(function(){
     $(this).addClass('bg-secondary')
     $(this).removeClass('bg-primary')
 })
